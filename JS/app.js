f8449874591d379320dd1ad8d7aa52b2bb6e9c01
andreaconv <andrea2convertini@gmail.com>
2023-03-23 16:29:46 +0100

    [edit] estrazone numeri

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,15 @@
 // DADI 
 
 document.getElementById("genera").addEventListener("click", function(){
-  let numero = Math.ceil(Math.random() * 6 )
-  console.log(numero)
+  let utente = Math.ceil(Math.random() * 6 )
+  
+  document.getElementById("utente").innerHTML = `
+  Il tuo numero è ${utente}  
+  `
+  let computer = Math.ceil(Math.random() * 6)
 
+  document.getElementById("computer").innerHTML = `
+  Il mio numero è ${computer}
+  `
 
 })
