cee292af6845e2536932870527640f400131c670
andreaconv <andrea2convertini@gmail.com>
2023-03-23 16:06:24 +0100

    [add] bozza progetti

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+// DADI 
+
+document.getElementById("genera").addEventListener("click", function(){
+  let numero = Math.floor(Math.random() * 10)
+  console.log(numero)
+
+
+})
---
+++ JS/script.js
@@ -0,0 +1,12 @@
+// ELENCO EMAIL 
+const arremail = [
+  'andrea@andrea.andrea',
+  'email@email.email'
+];
+
+document.getElementById("invia").addEventListener("click", function(){
+
+  const email = document.getElementById("email").value;
+  console.log(email);
+
+})
