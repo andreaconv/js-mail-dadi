56e3323ca897f2c55872e8287793b4a5db7d4906
andreaconv <andrea2convertini@gmail.com>
2023-03-23 17:57:47 +0100

    [add] modifiche

--- JS/script.js
+++ JS/script.js
@@ -9,4 +9,24 @@ document.getElementById("invia").addEventListener("click", function(){
   const email = document.getElementById("email").value;
   console.log(email);
 
+  for (let i = 0; i < arremail.length; i++){
+
+    if (email == arremail[i]) {
+      // le email sono corrette
+    }
+  }
+
+})
+
+
+
+// DADI 
+
+document.getElementById("genera").addEventListener("click", function(){
+  let utente = Math.ceil(Math.random() * 6 )  
+  document.getElementById("utente").innerHTML = `${utente}`
+
+  let computer = Math.ceil(Math.random() * 6)
+  document.getElementById("computer").innerHTML = `${computer}`
+
 })
